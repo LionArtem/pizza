ff6568c3bd88e3788432c4b883bf845b00bf4b8f
Grinevich Artem <grinevitch.artemy@yandex.ru>
2023-10-01 15:01:34 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 
 import './scss/app.scss';
 
-import Header from './components/Header';
 import Home from './pages/Home';
 import NodFaund from './pages/NodFaund';
 import Cart from './pages/Cart';
